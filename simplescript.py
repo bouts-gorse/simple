ac6ad982c202b9d302a2292a117b43ab6e923e3e
Marvin Morris <152704879+bouts-gorse@users.noreply.github.com>
2023-12-03 16:01:43 +0300

    Create simplescript.py

---
+++ simplescript.py
@@ -0,0 +1,13 @@
+# Simple Python Script
+
+# Prompt the user for their name
+name = input("Enter your name: ")
+
+# Prompt the user for their age
+age = int(input("Enter your age: "))
+
+# Calculate the age after 10 years
+future_age = age + 10
+
+# Greet the user and display their age after 10 years
+print(f"Hello, {name}! In 10 years, you'll be {future_age} years old.")
